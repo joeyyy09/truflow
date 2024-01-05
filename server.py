0da831ea74cff88f723ccc85a7251ff875bbd85f
GLVSKiriti <glvskiriti2003369@gmail.com>
2024-01-05 22:30:01 +0530

    Task1: Half duplex communication of simple messages between client and server is done
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- server.py
+++ server.py
@@ -1,29 +1,30 @@
 import socket
 import threading
 
-# Function to handle communication with a connected client
-def handle_client(client_socket):
-    while True:
-        try:
-            data = client_socket.recv(1024).decode('utf-8')
+SERVER_IP = "0.0.0.0"
+PORT = 5555
+
+# Function to receive messages from the friend
+def receive_messages(client_socket):
+    try:
+        data = client_socket.recv(1024).decode('utf-8')
             
-            # Check if data is empty, indicating the client has closed the connection
-            if not data:
-                print("\nConnection closed.")
-                break
-            
-            # Display the received message from the client
-            print(f"Friend: {data}")
-        except (socket.error, KeyboardInterrupt):
+        # Check if data is empty, indicating the client has closed the connection
+        if not data:
             print("\nConnection closed.")
-            break
-        
-        try:
-            message = input("You: ")
-            client_socket.send(message.encode('utf-8'))
-        except (socket.error, KeyboardInterrupt):
-            print("\nConnection closed.")
-            break
+ 
+        # Display the received message from the client
+        print(f"Friend: {data}")
+    except (socket.error, KeyboardInterrupt):
+        print("\nConnection closed.")
+
+
+def send_messages(client_socket):
+    try:
+        message = input("You: ")
+        client_socket.send(message.encode('utf-8'))
+    except (socket.error, KeyboardInterrupt):
+        print("\nConnection closed.")
 
 # Function to start the server
 def start_server():
@@ -31,20 +32,24 @@ def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     # Bind the server to the specified IP and port
-    server.bind(('0.0.0.0', 5555))
+    server.bind((SERVER_IP, PORT))
     
-
     server.listen(1)
     
-    print("Server listening on port 5555")
+    print(f"Server listening on port {PORT}")
 
     # Accept a client connection
     client, addr = server.accept()
     print(f"Accepted connection from {addr}")
 
-    # Start a thread to handle communication with the connected client
-    client_handler = threading.Thread(target=handle_client, args=(client,))
-    client_handler.start()
+    thread = 0
+    while True:
+        if thread == 1:
+            send_messages(client)
+            thread = 0
+        else:
+            receive_messages(client) 
+            thread = 1
 
 if __name__ == "__main__":
     start_server()
