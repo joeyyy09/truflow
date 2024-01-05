7d458d192a4b3328d58da139b1f124cbdf54750b
Joeyyy09 <imt_2021063@iiitm.ac.in>
2024-01-05 11:16:45 +0530

    Client Server Chat setup

--- client.py
+++ client.py
@@ -1,25 +1,36 @@
 import socket
 import threading
-
-def send_message(client_socket):
-    while True:
-        message = input("You: ")
-        client_socket.send(message.encode('utf-8'))
+import sys
 
 def receive_messages(client_socket):
     while True:
-        data = client_socket.recv(1024).decode('utf-8')
-        print(f"Friend: {data}")
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            print(f"Friend: {data}")
+        except (socket.error, KeyboardInterrupt):
+            print("\nConnection closed.")
+            break
+        sys.stdout.write("You: ")
+        sys.stdout.flush()
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 5555))
 
-    send_thread = threading.Thread(target=send_message, args=(client,))
+    friend_ip = input("Enter friend's IP address: ")
+    friend_port = int(input("Enter friend's port: "))
+
+    client.connect((friend_ip, friend_port))
+
     receive_thread = threading.Thread(target=receive_messages, args=(client,))
-
-    send_thread.start()
     receive_thread.start()
+
+    try:
+        while True:
+            message = input("You: ")
+            client.send(message.encode('utf-8'))
+    except (socket.error, KeyboardInterrupt):
+        print("\nConnection closed.")
+        client.close()
 
 if __name__ == "__main__":
     start_client()
