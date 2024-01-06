165ab7fc97f25897aa8368fd2c5eaba6cd54aeb0
GLVSKiriti <116095646+GLVSKiriti@users.noreply.github.com>
2024-01-06 22:12:59 +0530

    Half duplex communication between server and client is done (#7)
    * Task1: Half duplex communication of simple messages between client and server is done
    
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>
    
    * No need of threading module for now
    
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>
    
    ---------
    
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- client.py
+++ client.py
@@ -1,19 +1,26 @@
 import socket
-import threading
-import sys
 
 # Function to receive messages from the friend
 def receive_messages(client_socket):
-    while True:
-        try:
-            # Receive data from the friend and decode it
-            data = client_socket.recv(1024).decode('utf-8')
-            print(f"Friend: {data}")
-        except (socket.error, KeyboardInterrupt):
+    try:
+        data = client_socket.recv(1024).decode('utf-8')
+            
+        # Check if data is empty, indicating the client has closed the connection
+        if not data:
             print("\nConnection closed.")
-            break
-        sys.stdout.write("You: ")
-        sys.stdout.flush()
+ 
+        # Display the received message from the client
+        print(f"Friend: {data}")
+    except (socket.error, KeyboardInterrupt):
+        print("\nConnection closed.")
+
+
+def send_messages(client_socket):
+    try:
+        message = input("You: ")
+        client_socket.send(message.encode('utf-8'))
+    except (socket.error, KeyboardInterrupt):
+        print("\nConnection closed.")
 
 # Function to start the client and establish a connection with the friend
 def start_client():
@@ -26,17 +33,14 @@ def start_client():
     # Connect to the friend's IP and port
     client.connect((friend_ip, friend_port))
 
-    # Start a thread to receive messages from the friend
-    receive_thread = threading.Thread(target=receive_messages, args=(client,))
-    receive_thread.start()
-
-    try:
-        while True:
-            message = input("You: ")
-            client.send(message.encode('utf-8'))
-    except (socket.error, KeyboardInterrupt):
-        print("\nConnection closed.")
-        client.close()
-
+    thread = 1
+    while True:
+        if thread == 1:
+            send_messages(client)
+            thread = 0
+        else:
+            receive_messages(client) 
+            thread = 1
+ 
 if __name__ == "__main__":
     start_client()
