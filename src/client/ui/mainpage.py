32435bbe6a53f865aa550b6743998299200061c3
GLVSKiriti <glvskiriti2003369@gmail.com>
2024-01-23 20:59:37 +0530

    corrected a typo
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- src/client/ui/mainpage.py
+++ src/client/ui/mainpage.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_MainPageWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
