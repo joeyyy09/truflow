3099c204e1e363ad3beaa999d30710546c09659c
Joeyyy09 <imt_2021063@iiitm.ac.in>
2024-01-31 00:28:23 +0530

    Clients connecting to server
    Signed-off-by: Joeyyy09 <imt_2021063@iiitm.ac.in>

--- src/client/client.py
+++ src/client/client.py
@@ -8,66 +8,6 @@ FILE_NAME_SIZE = 1024
 FILE_SIZE_SIZE = 8
 CHUNK_SIZE = 4096
 
-# Use a queue for thread-safe communication
-message_queue = Queue()
-
-def receive_messages(client_socket: socket.socket) -> int:
-    try:
-        while True:
-            data: str = client_socket.recv(BUFFER_SIZE).decode('utf-8')
-            # Display the received message from the client
-            print(f"Friend: {data}")
-    except (socket.error, KeyboardInterrupt):
-        print("\nConnection closed.")
-        return 0
-
-def send_messages(client_socket: socket.socket) -> int:
-    try:
-        while True:
-            message = input("You: ")
-            client_socket.send(message.encode('utf-8'))
-    except (socket.error, KeyboardInterrupt):
-        print("\nConnection closed.")
-        return 0
-
-def send_files(client_socket: socket.socket):
-    try:
-        file_name = input("Enter the file name to be sent: ")
-
-        # file name and file size are padded with null values for fixed length
-        client_socket.send(file_name.ljust(FILE_NAME_SIZE, '\x00').encode())
-        client_socket.send(str(os.path.getsize(file_name)).ljust(FILE_SIZE_SIZE, '\x00').encode())
-
-        # send the file completely
-        with open(file_name, "rb") as file:
-            sendfile = file.read()
-            client_socket.sendall(sendfile)
-        print("Data has been sent successfully")
-
-    except Exception as e:
-        print(f"Error sending file: {e}")
-
-def receive_files(client_socket: socket.socket):
-    try:
-        file_name: str = client_socket.recv(FILE_NAME_SIZE).decode().rstrip('\x00')
-        file_size = int(client_socket.recv(FILE_SIZE_SIZE).decode().rstrip('\x00'))
-
-        received_data = b""
-        while len(received_data) < file_size:
-            chunk = client_socket.recv(CHUNK_SIZE)
-            if not chunk:
-                break
-            received_data += chunk
-
-        os.makedirs('recv', exist_ok=True)
-
-        with open(f'recv/{file_name}', 'wb') as file:
-            file.write(received_data)
-
-        print("Data has been received successfully")
-
-    except Exception as e:
-        print(f"Error receiving file: {e}")
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,36 +17,6 @@ def start_client():
 
     client.connect((server_ip, server_port))
 
-    while True:
-        print("\nChoose an option:")
-        print("1. Send a file")
-        print("2. Receive a file")
-        print("3. Chat")
-        print("4. Exit")
-
-        choice = input("Enter your choice (1/2/3/4): ")
-
-        if choice == "1":
-            send_files(client)
-        elif choice == "2":
-            receive_files(client)
-        elif choice == "3":
-            chat_thread = threading.Thread(target=receive_messages, args=(client,), daemon=True)
-            chat_thread.start()
-
-            send_thread = threading.Thread(target=send_messages, args=(client,), daemon=True)
-            send_thread.start()
-
-            chat_thread.join()
-            send_thread.join()
-
-        elif choice == "4":
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
-
-    client.close()
 
 if __name__ == "__main__":
     start_client()
--- src/server/server.py
+++ src/server/server.py
@@ -13,52 +13,7 @@ CHUNK_SIZE = 4096
 
 # Dictionary to store the status of each client
 client_sockets = {}
-shared_files = {}
 
-def get_shared_files_list():
-    shared_files_list = []
-    for client_socket in shared_files:
-        shared_files_list.extend(shared_files[client_socket])
-    return list(set(shared_files_list))  # Remove duplicates using set
-
-def accept_connections(server: socket.socket):
-    while True:
-        # Accept a client connection
-        client, addr = server.accept()
-        print(f"Accepted connection from {addr}")
-
-        # Start a thread to handle communication with the connected client
-        communication_thread = threading.Thread(target=handle_communication, args=(client,))
-        communication_thread.start()
-
-def handle_communication(client_socket: socket.socket):
-    try:
-        while True:
-            # Trim the null values and decode the file name and file size
-            file_name: str = client_socket.recv(FILE_NAME_SIZE).decode().rstrip('\x00')
-            file_size = int(client_socket.recv(FILE_SIZE_SIZE).decode().rstrip('\x00'))
-
-            # data is received in chunks and writes in the file
-            received_data = b""
-            while len(received_data) < file_size:
-                chunk = client_socket.recv(CHUNK_SIZE)
-                if not chunk:
-                    break
-                received_data += chunk
-
-            # creates a recv directory if not exists
-            os.makedirs('recv', exist_ok=True)
-
-            with open(f'recv/{file_name}', 'wb') as file:
-                file.write(received_data)
-
-            print("Data has been received successfully")
-
-    except Exception as e:
-        print(f"Error receiving file: {e}")
-
-    finally:
-        client_socket.close()
 
 def start_server():
     # Create a socket object for the server
@@ -84,47 +39,9 @@ def start_server():
         # Add the client socket to the dictionary
         client_sockets[addr] = client
 
-def send_files_to_client(client_addr, file_name):
-    try:
-        # Check if the file exists
-        if not os.path.exists(file_name):
-            print(f"Error: File '{file_name}' does not exist.")
-            return
 
-        file_size = str(os.path.getsize(file_name))
-
-        # file name and file size are padded with null values for fixed length
-        client_socket = client_sockets.get(client_addr)
-        if client_socket:
-            client_socket.send(file_name.ljust(FILE_NAME_SIZE, '\x00').encode())
-            client_socket.send(file_size.ljust(FILE_SIZE_SIZE, '\x00').encode())
-
-            # send the file completely
-            with open(file_name, "rb") as file:
-                sendfile = file.read()
-                client_socket.sendall(sendfile)
-                print("Data has been sent successfully")
-
-    except Exception as e:
-        print(f"Error sending file: {e}")
-
-def start_shared_files_thread():
-    while True:
-        # Send shared files list to all connected clients periodically
-        for client_socket in client_sockets.values():
-            try:
-                shared_file_list = get_shared_files_list()
-                client_socket.send(json.dumps(shared_file_list).encode('utf-8'))
-            except (socket.error, OSError):
-                pass
-
-        time.sleep(1)  # Optional: Adjust the sleep time between sending shared files list
 
 if __name__ == "__main__":
     server_thread = threading.Thread(target=start_server, daemon=True)
     server_thread.start()
-
-    shared_files_thread = threading.Thread(target=start_shared_files_thread, daemon=True)
-    shared_files_thread.start()
-
     server_thread.join()
