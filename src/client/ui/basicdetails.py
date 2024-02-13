4cc49a4d2664ce60d3e15847ea86534ff883713f
Harshith Mente <109957201+joeyyy09@users.noreply.github.com>
2024-02-13 23:46:45 +0530

    Update frontend according to the client functinos (#37)
    Signed-off-by: Joeyyy09 <imt_2021063@iiitm.ac.in>

--- src/client/ui/basicdetails.py
+++ src/client/ui/basicdetails.py
@@ -9,46 +9,41 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-# Appending parent sibling directories (packages) to import them
+import socket
 import pathlib
 import sys
+import mainpage  # Importing mainpage module
 
 parent_directory_src = str(pathlib.Path(__file__).parent.resolve().parents[1])
 sys.path.append(parent_directory_src)
 sys.path.append(parent_directory_src+"/server")
 
-
-import mainpage
-
-class Ui_BasicDetailsWindow(object):
+class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(484, 403)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(300, 200, 93, 28))
-        self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(100, 250, 291, 28))
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 230, 291, 28))
         self.pushButton_2.setObjectName("pushButton_2")
 
-        self.pushButton_2.clicked.connect(lambda: self.basicDetails_On_Continue(MainWindow))
+        # Connect the button click to the appropriate function
+        self.pushButton_2.clicked.connect(self.basicDetails_On_Continue)
 
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(100, 100, 55, 16))
+        self.label.setGeometry(QtCore.QRect(100, 160, 55, 16))
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(100, 170, 121, 16))
-        self.label_2.setObjectName("label_2")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(100, 130, 291, 28))
+        self.lineEdit.setGeometry(QtCore.QRect(100, 180, 291, 28))
         self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(100, 200, 191, 28))
-        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(100, 110, 291, 28))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(100, 90, 91, 16))
+        self.label_3.setObjectName("label_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -60,17 +55,27 @@ class Ui_BasicDetailsWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Basic Details"))
-        self.pushButton.setText(_translate("MainWindow", "Open"))
         self.pushButton_2.setText(_translate("MainWindow", "Continue"))
         self.label.setText(_translate("MainWindow", "Server IP"))
-        self.label_2.setText(_translate("MainWindow", "Share Folder Path"))
+        self.label_3.setText(_translate("MainWindow", "Username"))
+
 
     def basicDetails_On_Continue(self, MainWindow):
         print(self.lineEdit.text()) #ToDo: To store username and move to next 
-        print(self.lineEdit_2.text()) #ToDo: To store username and move to next 
+        print(self.lineEdit_3.text()) #ToDo: To store username and move to next 
 
         self.window = QtWidgets.QMainWindow()
         self.ui = mainpage.Ui_MainPageWindow()
         self.ui.setupUi(self.window)
         self.window.show()
-        MainWindow.close()
+        self.MainWindow.close()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    ui.MainWindow = MainWindow
+    sys.exit(app.exec_())
